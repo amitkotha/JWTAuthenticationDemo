d2ec668c669aa70745b2db1d3793618cfa2c842a
amitkotha <amitkotha1989@gmail.com>
2021-06-08 01:52:48 +0530

    Add project files.

---
+++ JWTAuthenticationDemo/AuthenticationResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace JWTAuthenticationDemo
+{
+    public class AuthenticationResponse
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string Username { get; set; }
+
+        public string Token { get; set; }
+
+        public AuthenticationResponse(User user,string token)
+        {
+            Id = user.Id;
+            FirstName = user.FirstName;
+            Username = user.Username;
+            Token = token;
+
+        }
+    }
+}
---
+++ JWTAuthenticationDemo/UserService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthenticationDemo
+{
+
+    public interface IUserService
+    {
+        AuthenticationResponse Authenticate(AuthenticateRequest model);
+       
+    }
+    public class UserService : IUserService
+    {
+
+        private readonly AppSettings _appSettings;
+        public UserService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+        private List<User> _users = new List<User>
+        {
+            new User { Id = 1, FirstName = "Test", Username = "test", Password = "test" }
+        };
+
+        public AuthenticationResponse Authenticate(AuthenticateRequest model)
+        {
+            var user = _users.SingleOrDefault(x => x.Username == model.Username && x.Password == model.Password);
+
+            // return null if user not found
+            if (user == null) return null;
+
+            // authentication successful so generate jwt token
+            var token = generateJwtToken(user);
+            return new AuthenticationResponse(user, token);
+        }
+
+        private string generateJwtToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor=new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        
+    }
+}
